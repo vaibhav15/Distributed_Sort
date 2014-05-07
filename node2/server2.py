3d32537b5b8adc32116841ebe305c015c036bad3
hardikj <hardikjuneja.hj@gmail.com>
2014-05-07 11:57:59 +0530

    adding fucking support for multiple node

--- node2/server2.py
+++ node2/server2.py
@@ -3,12 +3,21 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 import zmq
 
+import config
+
 # ZeroMQ Context
 context = zmq.Context()
 
+id = 1
+lconfig = config.config[id]
+lconfig = lconfig.itervalues().next()
+port = lconfig["port"]
+file = lconfig["file"]
+
 # Define the socket using the "Context"
 sock = context.socket(zmq.REP)
-sock.bind("tcp://*:5556")
+sock.bind("tcp://*:%s"%port)
+print("binding to port %s"%port)
 
 while True:
     message = sock.recv()
@@ -20,7 +29,7 @@ while True:
         sock.send("started")
 
     elif message[0] == "msg":
-        fo = open("dataB.txt", "a+")
+        fo = open(file, "a+")
         content = message[1]
         fo.write(content)
         sock.send("done")
